a786da1cdc768335d29037d9bf3fde880020aad9
Julien <gerard.julien2000@orange.fr>
2021-12-10 16:14:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import firebase from 'firebase'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyCey_v71gUqwxbC6Mb47-ZdB6tPHHgxwRQ",
+      authDomain: "crm-linkedin-e8177.firebaseapp.com",
+      projectId: "crm-linkedin-e8177",
+      storageBucket: "crm-linkedin-e8177.appspot.com",
+      messagingSenderId: "233163257383",
+      appId: "1:233163257383:web:e39f7c840d22d28248c002",
+      measurementId: "${config.measurementId}"
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
